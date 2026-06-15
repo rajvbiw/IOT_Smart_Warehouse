e31e302d22093bdd49e3932dd80cef438855eeb4
rajbirari800 <rajbirari800@gmail.com>
2026-06-16 02:43:50 +0530

    update terraform

--- terraform/environments/prod/imports.tf
+++ terraform/environments/prod/imports.tf
@@ -7,18 +7,3 @@ import {
   to = module.root.module.ecr.aws_ecr_repository.frontend
   id = "warehouse-iot-prod-frontend"
 }
-
-import {
-  to = module.root.module.iot_core.aws_iot_policy.device
-  id = "warehouse-iot-prod-device-policy"
-}
-
-import {
-  to = module.root.module.eks.aws_iam_role.ebs_csi
-  id = "warehouse-iot-prod-ebs-csi-role"
-}
-
-import {
-  to = module.root.module.iam.aws_iam_role.pod_role
-  id = "warehouse-iot-prod-pod-role"
-}
