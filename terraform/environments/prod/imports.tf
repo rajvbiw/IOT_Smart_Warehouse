81a996480d836d009374466291e06e75028c9982
rajbirari800 <rajbirari800@gmail.com>
2026-06-16 03:15:40 +0530

    fix: delete imports.tf since ECR repositories are destroyed

--- terraform/environments/prod/imports.tf
+++
@@ -1,9 +0,0 @@
-import {
-  to = module.root.module.ecr.aws_ecr_repository.backend
-  id = "warehouse-iot-prod-backend"
-}
-
-import {
-  to = module.root.module.ecr.aws_ecr_repository.frontend
-  id = "warehouse-iot-prod-frontend"
-}
